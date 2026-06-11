e8e82cddd072d7c0c4cadc8ece68366142e8588a
Victor <shulgasv1ls74.ru@mail.ru>
2026-06-11 12:57:18 +0500

    fix bg and add close modal

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,3 +1,19 @@
-function modalShow(key){
+function modalShow(key) {
+  modalClose();
   document.querySelector(key).style.display = 'flex';
 }
+
+function modalClose() {
+  document.querySelectorAll('.modal__container').forEach(modal => {
+    modal.style.display = 'none';
+  });
+}
+
+document.addEventListener('click', function (e) {
+  const modal = e.target.closest('.modal_close');
+
+  if (modal) {
+    e.preventDefault();
+    modalClose();
+  }
+});
